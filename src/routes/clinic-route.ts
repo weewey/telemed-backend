e83aaf9bff6acb36aee26d2515273730c6d5504c
yewwee <chuayewwee@gmail.com>
2021-04-25 18:14:53 +0800

    [Tech] simple formatting of clinic-route.ts

--- src/routes/clinic-route.ts
+++ src/routes/clinic-route.ts
@@ -6,15 +6,15 @@ export const clinicRoute = Router()
 
 clinicRoute.get("/:clinicId",
     asyncHandler(async (req: Request, res: Response): Promise<void> => {
-            const { clinicId } = req.params;
+            const {clinicId} = req.params;
             const clinicInfo = await ClinicService.getClinicById(clinicId)
             clinicInfo ? res.json(clinicInfo) : res.status(404).send();
         }
-));
+    ));
 
 clinicRoute.get("/",
     asyncHandler(async (req: Request, res: Response): Promise<void> => {
             const clinics = await ClinicService.getClinics();
             res.json(clinics);
-    }
-));
+        }
+    ));
