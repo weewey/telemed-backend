d2b3692c6002828e9e5f0c565c63c364034ff46b
yewwee <chuayewwee@gmail.com>
2021-05-07 23:30:52 +0800

    [47] fix linting issues

--- src/migrations/20210507140115-create-doctors.js
+++ src/migrations/20210507140115-create-doctors.js
@@ -59,7 +59,7 @@ module.exports = {
       }
     });
   },
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface) => {
     await queryInterface.dropTable('Doctors');
   }
 };
