2cf5b32ba5e97acf6de46cf1259a5ec512af5ed1
yewwee <chuayewwee@gmail.com>
2021-05-08 20:49:03 +0800

    [47] Add tests in doctor.test.ts for UniqueConstraintError

--- __tests__/models/doctor.test.ts
+++ __tests__/models/doctor.test.ts
@@ -1,6 +1,6 @@
 import Doctor from "../../src/models/doctor";
 import {clinicFactory} from "../factories";
-import {ValidationError} from "sequelize";
+import {UniqueConstraintError, ValidationError} from "sequelize";
 import QueueStatus from "../../src/queue_status";
 import Queue from "../../src/models/queue";
 import {v4 as generateUUID} from "uuid";
@@ -8,10 +8,11 @@ import {DoctorAttributes} from "../../src/services/doctor-service";
 
 describe("Doctor", () => {
     const doctorIdsToBeDeleted: Array<number> = []
-    const getDoctorAttrs = (): DoctorAttributes => {
+    const getDoctorAttrs = (overrideAttrs?: Partial<DoctorAttributes>): DoctorAttributes => {
         return {
             authId: generateUUID(), email: `${generateUUID()}@gmail.com`, firstName: "first name",
-            lastName: "last", mobileNumber: generateUUID(), onDuty: false
+            lastName: "last", mobileNumber: generateUUID(), onDuty: false,
+            ...overrideAttrs
         }
     }
 
@@ -49,11 +50,7 @@ describe("Doctor", () => {
 
 
             it("should associate correctly to queue", async () => {
-                try {
-                    doctor = await Doctor.create({...getDoctorAttrs(), queueId: queueId})
-                } catch (e) {
-                    console.log(JSON.stringify(e))
-                }
+                doctor = await Doctor.create(getDoctorAttrs({queueId: queueId}))
 
                 const foundDoctor = await Doctor.findOne({where: {id: doctor.id}, include: Queue})
                 expect(foundDoctor?.queue?.id).toEqual(queueId)
@@ -63,32 +60,64 @@ describe("Doctor", () => {
         });
     })
 
-    describe('invalid', () => {
+    describe('when invalid', () => {
         it('should return an error when the email is not valid', async () => {
-            await expect(Doctor.create({...getDoctorAttrs(), email: "email"})).rejects
+            await expect(Doctor.create(getDoctorAttrs({email: "email"}))).rejects
                 .toThrowError(ValidationError)
         });
 
         it('should return an error when the firstName is not valid', async () => {
-            await expect(Doctor.create({...getDoctorAttrs(), firstName: "1234"})).rejects
+            await expect(Doctor.create(getDoctorAttrs({firstName: "1234"}))).rejects
                 .toThrowError(ValidationError)
         });
 
         it('should return an error when the firstName is above 50 characters', async () => {
-            await expect(Doctor.create({...getDoctorAttrs(), firstName:
-                    "Lorem ipsum dolor sit ameta consectetuer adipiscing"})).rejects
+            await expect(Doctor.create(
+                getDoctorAttrs({
+                    firstName:
+                        "Lorem ipsum dolor sit ameta consectetuer adipiscing"
+                })
+            )).rejects
                 .toThrowError(ValidationError)
         });
 
         it('should return an error when the lastName is above 50 characters', async () => {
-            await expect(Doctor.create({...getDoctorAttrs(), firstName:
-                    "Lorem ipsum dolor sit ameta consectetuer adipiscing"})).rejects
+            await expect(Doctor.create(
+                getDoctorAttrs({firstName: "Lorem ipsum dolor sit ameta consectetuer adipiscing"}),
+            )).rejects
                 .toThrowError(ValidationError)
         });
 
         it('should return an error when the lastName is not valid', async () => {
-            await expect(Doctor.create({...getDoctorAttrs(), lastName: "1234"})).rejects
+            await expect(Doctor.create(getDoctorAttrs({lastName: "1234"}))).rejects
                 .toThrowError(ValidationError)
+        });
+
+        describe('when fields are not unique', () => {
+            const email = "123@gmail.com"
+            const authId = "authId"
+            const mobileNumber = "123456"
+
+            beforeAll(async () => {
+                const doctor = await Doctor.create(getDoctorAttrs({email, authId, mobileNumber}))
+
+                doctorIdsToBeDeleted.push(doctor.id)
+
+            })
+            it('should return UniqueConstraintError when email is not unique', async () => {
+                await expect(Doctor.create(getDoctorAttrs({email})))
+                    .rejects.toThrowError(UniqueConstraintError)
+            });
+
+            it('should return UniqueConstraintError when authId is not unique', async () => {
+                await expect(Doctor.create(getDoctorAttrs({authId})))
+                    .rejects.toThrowError(UniqueConstraintError)
+            });
+
+            it('should return UniqueConstraintError when mobileNumber is not unique', async () => {
+                await expect(Doctor.create(getDoctorAttrs({mobileNumber})))
+                    .rejects.toThrowError(UniqueConstraintError)
+            });
         });
     });
 });
