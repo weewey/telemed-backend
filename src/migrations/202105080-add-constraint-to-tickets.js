1a23ca2fa643a6912e8a4074ea65891df2468716
aaron <hau.zhongyuan@gmail.com>
2021-05-10 22:27:39 +0800

    add constraints for composite key to ticket model

---
+++ src/migrations/202105080-add-constraint-to-tickets.js
@@ -0,0 +1,23 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface) => {
+    return queryInterface.sequelize.transaction(t => {
+      return Promise.all([
+        queryInterface.addConstraint("Tickets", {
+          type: "unique",
+          fields: ['queueId', 'displayNumber'],
+          name: "Tickets_queueId_displayNumber_unique_key"
+        }, { transaction: t })
+      ]);
+    });
+  },
+
+  down: async (queryInterface) => {
+    return queryInterface.sequelize.transaction((t) => {
+      return Promise.all([
+        queryInterface.removeConstraint("Tickets", "Tickets_queueId_displayNumber_unique_key", { transaction: t }),
+      ])
+    })
+  }
+};
