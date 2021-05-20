d232fa3d8fb65a4c8d07cd7c44a7b885e330a69a
yewwee <chuayewwee@gmail.com>
2021-05-20 21:06:41 +0800

    [22] attempt to use secrets

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,11 +13,37 @@ provider "google" {
   credentials = file(var.gcp_auth_file)
 }
 
+resource "google_secret_manager_secret" "qdoc_staging_db_password" {
+  provider = google
+
+  secret_id = "qdoc_staging_db_password"
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "qdoc_staging_db_password_data" {
+  provider = google
+
+  secret      = google_secret_manager_secret.qdoc_staging_db_password.secret_id
+  secret_data = var.db_database
+}
+
+resource "google_secret_manager_secret_iam_member" "secret-access" {
+  provider = google
+
+  secret_id  = google_secret_manager_secret.qdoc_staging_db_password.secret_id
+  role       = "roles/secretmanager.secretAccessor"
+  member     = "serviceAccount:${var.project_id}-compute@developer.gserviceaccount.com"
+  depends_on = [google_secret_manager_secret.qdoc_staging_db_password]
+}
+
 resource "google_cloud_run_service" "qdoc" {
   name     = "qdoc-${var.environment}"
   location = var.region
 
   autogenerate_revision_name = true
+  depends_on                 = [google_secret_manager_secret_version.qdoc_staging_db_password_data]
 
   template {
     metadata {
@@ -30,7 +56,7 @@ resource "google_cloud_run_service" "qdoc" {
 
     spec {
       containers {
-        image = "asia.gcr.io/${var.project_id}/qdoc:latest"
+        image = "asia.gcr.io/${var.project_id}/qdoc"
         env {
           name  = "NODE_ENV"
           value = var.environment
@@ -40,8 +66,13 @@ resource "google_cloud_run_service" "qdoc" {
           value = var.db_host
         }
         env {
-          name  = "DB_DATABASE"
-          value = var.db_database
+          name = "DB_DATABASE"
+          value_from {
+            secret_key_ref {
+              name = google_secret_manager_secret.qdoc_staging_db_password.secret_id
+              key  = "1"
+            }
+          }
         }
         env {
           name  = "DB_PASSWORD"
