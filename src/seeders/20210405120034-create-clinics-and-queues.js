89a625a10b4b2b5d088dadf626c32540d4b63c12
SherlynnTan <1sherlynn@gmail.com>
2021-05-22 23:55:31 +0800

    [30] Add to seed migration - add queues

---
+++ src/seeders/20210405120034-create-clinics-and-queues.js
@@ -0,0 +1,58 @@
+module.exports = {
+  up: async (queryInterface) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.bulkInsert("Clinics", [ {
+        name: "Clinic name 1",
+        imageUrl: "http://image",
+        address: "Clinic Address 1",
+        lat: 1.30328,
+        long: 103.847049,
+        postalCode: "012345",
+        email: "email@email.com",
+        phoneNumber: "12345678",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Clinic name 2",
+        imageUrl: "http:image.url",
+        address: "Clinic Address 2",
+        lat: 1.30328,
+        long: -103.847049,
+        postalCode: "222222",
+        email: "email2@email.com",
+        phoneNumber: "12345679",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      ], { transaction: t });
+      const clinic = await queryInterface.sequelize.query("SELECT id FROM \"Clinics\" limit 1;");
+      await queryInterface.bulkInsert("Queues", [ {
+        clinicId: clinic[0][0].id,
+        status: "INACTIVE",
+        waitingTicketsCount: 0,
+        waitingTicketsId: "{}",
+        closedTicketsId: "{}",
+        latestGeneratedTicketDisplayNumber: 0,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        clinicId: clinic[0][0].id,
+        status: "INACTIVE",
+        waitingTicketsCount: 0,
+        waitingTicketsId: "{}",
+        closedTicketsId: "{}",
+        latestGeneratedTicketDisplayNumber: 0,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      ], { transaction: t });
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.bulkDelete("Clinics", null, {});
+    await queryInterface.bulkDelete("Queues", null, {});
+  },
+};
