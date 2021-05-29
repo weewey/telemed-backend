28f253e8a9fc764022e8a72e86e1c4ebc76652c0
yewwee <chuayewwee@gmail.com>
2021-05-29 15:15:51 +0800

    [20] add delete at PatientService

--- __tests__/services/patient-service.test.ts
+++ __tests__/services/patient-service.test.ts
@@ -1,9 +1,9 @@
 import Patient from "../../src/models/patient";
-import patientService from "../../src/services/patient-service";
 import patientRepository, { PatientAttributes } from "../../src/respository/patient-repository";
 import TechnicalError from "../../src/errors/technical-error";
 import RepositoryError from "../../src/errors/repository-error";
 import { Errors } from "../../src/errors/error-mappings";
+import PatientService from "../../src/services/patient-service";
 
 describe("Patient service", () => {
   describe("#create", () => {
@@ -16,7 +16,7 @@ describe("Patient service", () => {
     };
     it("should call patient repository #create", async () => {
       jest.spyOn(patientRepository, "create").mockResolvedValue({} as Patient);
-      await patientService.create(patientAttributes);
+      await PatientService.create(patientAttributes);
 
       expect(patientRepository.create).toHaveBeenCalledTimes(1);
       expect(patientRepository.create).toHaveBeenCalledWith(patientAttributes);
@@ -26,7 +26,7 @@ describe("Patient service", () => {
       it("should throw TechnicalError when patientRepository.create throw unknown error", async () => {
         jest.spyOn(patientRepository, "create").mockRejectedValueOnce(new TechnicalError());
 
-        await expect(patientService.create(patientAttributes)).rejects.toThrowError(TechnicalError);
+        await expect(PatientService.create(patientAttributes)).rejects.toThrowError(TechnicalError);
       });
 
       it("should throw FIELD_ALREADY_EXISTS error when repo throws error with FIELD_ALREADY_EXISTS", async () => {
@@ -34,7 +34,7 @@ describe("Patient service", () => {
           new RepositoryError(Errors.FIELD_ALREADY_EXISTS.code, "field alry exists"),
         );
 
-        await expect(patientService.create(patientAttributes)).rejects.toMatchObject(
+        await expect(PatientService.create(patientAttributes)).rejects.toMatchObject(
           { code: Errors.FIELD_ALREADY_EXISTS.code,
             message: "field alry exists" },
         );
@@ -45,10 +45,29 @@ describe("Patient service", () => {
           new RepositoryError(Errors.VALIDATION_ERROR.code, "field validation error"),
         );
 
-        await expect(patientService.create(patientAttributes)).rejects.toMatchObject(
+        await expect(PatientService.create(patientAttributes)).rejects.toMatchObject(
           { code: Errors.VALIDATION_ERROR.code,
             message: "field validation error" },
         );
+      });
+    });
+  });
+
+  describe("#destroy", () => {
+    const mockPatient = { id: 1, destroy: () => {} } as Patient;
+
+    it("should call patient.destroy", async () => {
+      const patientSpy = jest.spyOn(mockPatient, "destroy").mockResolvedValue(undefined);
+      await PatientService.delete(mockPatient);
+      expect(patientSpy).toBeCalled();
+    });
+
+    describe("when destroy returns an error", () => {
+      it("should throw TechnicalError", async () => {
+        jest.spyOn(mockPatient, "destroy").mockRejectedValue(new Error("test"));
+        await expect(PatientService.delete(mockPatient))
+          .rejects
+          .toEqual(new TechnicalError("Error deleting patient record: 1. test"));
       });
     });
   });
--- src/services/patient-service.ts
+++ src/services/patient-service.ts
@@ -1,6 +1,8 @@
 import Patient from "../models/patient";
 import PatientRepository, { PatientAttributes } from "../respository/patient-repository";
 import { mapRepositoryErrors } from "./helpers/handle-repository-errors";
+import { Logger } from "../logger";
+import TechnicalError from "../errors/technical-error";
 
 class PatientService {
   public static async create(patientAttributes: PatientAttributes): Promise<Patient> {
@@ -8,6 +10,16 @@ class PatientService {
       return await PatientRepository.create(patientAttributes);
     } catch (error) {
       throw mapRepositoryErrors(error);
+    }
+  }
+
+  public static async delete(patient: Patient): Promise<void> {
+    try {
+      return await patient.destroy();
+    } catch (error) {
+      const errorMessage = `Error deleting patient record: ${patient.id}. ${error.message}`;
+      Logger.error(errorMessage);
+      throw new TechnicalError(errorMessage);
     }
   }
 }
