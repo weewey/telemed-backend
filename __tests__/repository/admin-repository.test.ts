4ad1d65210e1fb22697d3cd12a3e79d6517383a0
yewwee <chuayewwee@gmail.com>
2021-06-12 15:21:01 +0800

    [Tech][YewWee] add adminRepository

---
+++ __tests__/repository/admin-repository.test.ts
@@ -0,0 +1,90 @@
+import { v4 as generateUUID } from "uuid";
+import AdminRepository, { AdminAttributes } from "../../src/respository/admin-repository";
+import { ForeignKeyConstraintError, UniqueConstraintError, ValidationError, ValidationErrorItem } from "sequelize";
+import RepositoryError from "../../src/errors/repository-error";
+import { Logger } from "../../src/logger";
+import { Errors } from "../../src/errors/error-mappings";
+import Admin from "../../src/models/admins";
+import objectContaining = jasmine.objectContaining;
+
+describe("Admin Repository", () => {
+  const getAdminAttrs = (overrideAttrs?: Partial<AdminAttributes>): AdminAttributes => {
+    return {
+      authId: generateUUID(),
+      email: `${generateUUID()}@gmail.com`,
+      firstName: "first name",
+      lastName: "last",
+      mobileNumber: generateUUID(),
+      ...overrideAttrs,
+    };
+  };
+
+  beforeEach(() => {
+    jest.spyOn(Logger, "error").mockImplementation(() => {});
+  });
+
+  describe("success", () => {
+    it("should call Admin with the expected attributes", async () => {
+      const adminAttrs = getAdminAttrs();
+      const spy = jest.spyOn(Admin, "create").mockResolvedValue();
+      await AdminRepository.create(adminAttrs);
+      expect(spy).toBeCalledWith(adminAttrs);
+    });
+  });
+
+  describe("errors", () => {
+    describe("when it encounters UniqueConstraintError", () => {
+      it("should return repository error", async () => {
+        const adminAttrs = getAdminAttrs();
+        jest.spyOn(Admin, "create").mockRejectedValue(
+          new UniqueConstraintError({
+            errors: [ new ValidationErrorItem("email must be unique",
+              "email", "email") ],
+          }),
+        );
+        await expect(AdminRepository.create(adminAttrs)).rejects.toThrowError(RepositoryError);
+      });
+
+      it("should return the expected error code", async () => {
+        const adminAttrs = getAdminAttrs();
+        jest.spyOn(Admin, "create").mockRejectedValue(
+          new UniqueConstraintError({
+            errors: [ new ValidationErrorItem("email must be unique",
+              "email", "email") ],
+          }),
+        );
+        await expect(AdminRepository.create(adminAttrs)).rejects
+          .toMatchObject(objectContaining({
+            code: Errors.FIELD_ALREADY_EXISTS.code,
+          }));
+      });
+    });
+
+    describe("when it encounters ValidationError", () => {
+      it("should return repository error with the validation error code", async () => {
+        const adminAttrs = getAdminAttrs({ email: "123" });
+        jest.spyOn(Admin, "create").mockRejectedValue(
+          new ValidationError("Validation isEmail on email failed",
+            [ new ValidationErrorItem("email must be unique", "email") ]),
+        );
+        await expect(AdminRepository.create(adminAttrs)).rejects
+          .toMatchObject(objectContaining({
+            code: Errors.VALIDATION_ERROR.code,
+          }));
+      });
+    });
+
+    describe("when it encounters ForeignKeyConstraintError", () => {
+      it("should return repository error with associated entity not found error code", async () => {
+        const adminAttrs = getAdminAttrs({ email: "123" });
+        jest.spyOn(Admin, "create").mockRejectedValue(
+          new ForeignKeyConstraintError({ message: "clinicId 1 not found", fields: [ "clinicId" ] }),
+        );
+        await expect(AdminRepository.create(adminAttrs)).rejects
+          .toMatchObject(objectContaining({
+            code: Errors.ENTITY_NOT_FOUND.code,
+          }));
+      });
+    });
+  });
+});
--- src/respository/admin-repository.ts
+++ src/respository/admin-repository.ts
@@ -1,3 +1,10 @@
+import Admin from "../models/admins";
+import { BaseError, ForeignKeyConstraintError, UniqueConstraintError, ValidationError } from "sequelize";
+import { mapSequelizeErrorToErrorMessage } from "../utils/helpers";
+import { Errors } from "../errors/error-mappings";
+import RepositoryError from "../errors/repository-error";
+import { Logger } from "../logger";
+
 export interface AdminAttributes {
   firstName: string,
   lastName: string,
@@ -5,3 +12,36 @@ export interface AdminAttributes {
   authId: string,
   mobileNumber: string,
 }
+
+class AdminRepository {
+  public static async create(adminAttributes: AdminAttributes): Promise<Admin> {
+    let admin: Admin;
+    try {
+      admin = await Admin.create(adminAttributes);
+    } catch (error) {
+      throw this.handleCreateAdminError(error);
+    }
+    return admin;
+  }
+
+  private static handleCreateAdminError(error: BaseError): RepositoryError {
+    if (error instanceof UniqueConstraintError) {
+      const message = mapSequelizeErrorToErrorMessage("Unable to create admin", error.errors);
+      Logger.error(message);
+      throw new RepositoryError(Errors.FIELD_ALREADY_EXISTS.code, message);
+    }
+    if (error instanceof ValidationError) {
+      const message = mapSequelizeErrorToErrorMessage("Unable to create admin", error.errors);
+      Logger.error(message);
+      throw new RepositoryError(Errors.VALIDATION_ERROR.code, message);
+    }
+    if (error instanceof ForeignKeyConstraintError) {
+      const message = `Unable to create admin ${error.fields} ${error.message}}`;
+      Logger.error(message);
+      throw new RepositoryError(Errors.ENTITY_NOT_FOUND.code, message);
+    }
+    throw error;
+  }
+}
+
+export default AdminRepository;
